511be572c95ef89d3e9fedd01df9b5ada34cba9b
Adrian Iacob <adrian@oc2627300782.ibm.com>
2020-04-24 09:02:21 +0300

    updated the right test

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,9 @@
 sleep 40
-if curl http://app:8000 | grep -q 'You may need to add'; then
+if curl http://app:8000 | grep -q 'Home'; then
   echo "Tests passed!" > testresult
   exit 0
 else
   echo "Tests failed!" > testresult
   exit 1
 fi
+# curl -X GET -i -o /dev/null -s -w "%{http_code}\n" http://app:8000
